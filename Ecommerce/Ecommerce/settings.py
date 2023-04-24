ef4e70391fe8b82a6fe22a3db6e1bdfb8a3d5cde
williams <115151479+wiliz3d@users.noreply.github.com>
2023-04-24 01:32:31 +0100

    w

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -76,9 +76,11 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecommerce', 
+        'USER': 'ecommerce',
+        'PASSWORD': '1234',
     }
 }
 
