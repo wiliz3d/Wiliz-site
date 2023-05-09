91f53ffbb4f8bec6bfda82bff5ca062759c58bb1
wiliz <115151479+wiliz3d@users.noreply.github.com>
2023-05-09 19:24:31 +0100

    working

--- Ecommerce/main/models.py
+++ Ecommerce/main/models.py
@@ -1,24 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-class Customer(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=250, null=True)
-
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    
     def __str__(self):
-        return self.name
-    
-    
+        return self.email
+
 class Product(models.Model):
-    name = models.CharField(max_length=200, null=True)
-    price = models.FloatField()
-    image = models.ImageField(upload_to='covers')
-    digital = models.BooleanField(default=False,null=True,blank=True)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_images')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-       
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wishlists')
+    products = models.ManyToManyField(Product, related_name='wishlists')
+
+    def __str__(self):
+        return f"{self.user}'s Wishlist"
+
     
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,blank=True,null=True)
--- Ecommerce/main/views.py
+++ Ecommerce/main/views.py
@@ -1,60 +1,55 @@
-from django.shortcuts import * 
-from . models import *
-from django.conf import settings
+from django.contrib.auth import( authenticate, login )
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from .forms import(
-                    SignupForm,
-                    SignInForm
-                  )
-from django.contrib.auth import(
-                                authenticate,
-                                login
-                                )
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import *
 
 
-def Home(request):
-    products = Product.objects.all()
-    context = {'products':products}
-    return render(request, ('home.html'),context)
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'signup.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('home')
+        return super().dispatch(request, *args, **kwargs)
 
 
-def Signup(request):
-    if request.method =='POST':
-        user_form = SignupForm(request.POST)
-        if user_form.is_valid():
-            user_form.save()
-            return render(request,('home.html'),context)
-        else:
-            user_form = SignupForm()
-        return HttpResponse('<h1>Error 404</h1>')
-    context = {'user_form':user_form}
-    return render(request, 'accounts/signup.html',context)
-
-
-def Signin(request):
+def signin(request):
+    error = ''
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method == 'POST':
-        form = SignInForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user=authenticate(request,
-                              username=cd["username"],
-                              password=cd['password']
-                              )
-            if user is not None:
-                if user.is_active:
-                    login(request,user)
-                    return HttpResponse("<h1>Wlcome</h2>")
-                else:
-                    return HttpResponse("<h1>DIssable<h1/>")
-            else:
-                return HttpResponse("<h1>INVALID<h1/>")
-    else:
-        form = SignInForm()            
-    return render(request, 'accounts/signin.html',{"form"})
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error = 'Invalid username or password.'
+    return render(request, 'signin.html', {'error': error})
 
 
+@login_required
+def wishlist(request):
+    wishlist = Wishlist.objects.get_or_create(user=request.user)[0]
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')
+        if product_id:
+            product = Product.objects.get(id=product_id)
+            wishlist.products.add(product)
+            return redirect('wishlist')
+    products = wishlist.products.all()
+    return render(request, 'wishlist.html', {'products': products})
 
-def About(request):
-    return render(request, ('About.html'),{})
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
+
 
